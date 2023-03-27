b99e3f657836afa5dcbc126e2c0364542fbb1835
Kaua <111804830+kauah88@users.noreply.github.com>
2023-03-27 17:12:24 -0300

    Add files via upload

---
+++ atividade_alg.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+
+	
+    float custo, frete, despesas, venda, lucro;
+    
+    printf("Digite o custo da mercadoria: ");
+    scanf("%f", &custo);
+    
+    printf("Digite o valor do frete: ");
+    scanf("%f", &frete);
+    
+    printf("Digite o valor das despesas eventuais: ");
+    scanf("%f", &despesas);
+
+    custo = custo + frete + despesas;
+    
+    printf("Digite o valor de venda: ");
+    scanf("%f", &venda);
+    
+    lucro = (venda - custo - frete - despesas) / custo * 100;
+    
+    printf("A porcentagem de lucro da mercadoria: %.2f%%", lucro);
+    
+    return 0;
+    
+}
